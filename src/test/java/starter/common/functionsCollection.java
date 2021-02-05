c4b51cb87ea8b45aff2c1cb34e2bc37764ba5cd4
Pranay <pranay85singh@hotmail.com>
2021-02-05 20:02:28 +1300

    Final Commit

--- src/test/java/starter/common/functionsCollection.java
+++ src/test/java/starter/common/functionsCollection.java
@@ -3,57 +3,32 @@ package starter.common;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.type.Type;
-import org.openqa.selenium.Keys;
-import starter.pages.registrationForm;
+import net.serenitybdd.screenplay.targets.Target;
+
 
 public class functionsCollection {
     public static Performable TextBox(String parameter, String value) {
         return Task.where("{0} Enters values for '" + parameter + "' as '"+value+"'",
+
                 Type.theValue(value)
-                        .into(registrationForm.BusinessName_Field),
-                Type.theValue(value)
-                        .into(registrationForm.EntityID_Field),
-                Type.theValue(value)
-                        .into(registrationForm.Address1_Field),
-                Type.theValue(value)
-                        .into(registrationForm.City_Field),
-                Type.theValue(value)
-                        .into(registrationForm.State_Field),
-                Type.theValue(value)
-                        .into(registrationForm.PostCode_Field),
-                Type.theValue(value)
-                        .into(registrationForm.MainPhone_Field),
-                Type.theValue(value)
-                        .into(registrationForm.WebSite_Field),
-                Type.theValue(value)
-                        .into(registrationForm.FirstName_Field),
-                Type.theValue(value)
-                        .into(registrationForm.LastName_Field),
-                Type.theValue(value)
-                        .into(registrationForm.DirectPhone_Field),
-                Type.theValue(value)
-                        .into(registrationForm.Email_Field)
+                        .into(Target.the(parameter)
+                                .locatedBy("#"+parameter))
                 );
     }
     public static Performable DropDown(String parameter, String value) {
         return Task.where("{0} Selects values for '" + parameter + "' as '"+value+"'",
 
                 Type.theValue(value)
-                        .into(registrationForm.Country_Field)
-                        .thenHit(Keys.ENTER),
-                Type.theValue(value)
-                        .into(registrationForm.HearAbout_Field)
-                        .thenHit(Keys.ENTER),
-                Type.theValue(value)
-                        .into(registrationForm.IndustryType_Field)
-                        .thenHit(Keys.ENTER)
+                        .into(Target.the(parameter)
+                                .locatedBy("#"+parameter))
         );
     }
 
     public static Performable Button(String parameter, String value) {
         return Task.where("{0} Clicks on '" + parameter + "' for '"+value+"'"
-                //Below code has been commented as application does not need to submitted.
-//                Click.on(registrationForm.SingUpButton_Field)
+//                //Below code has been commented as application does not need to submitted.
+//                Click.on(Target.the(parameter)
+//                                .locatedBy("#"+parameter))
         );
     }
 
